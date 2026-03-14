9370b93674b8cc694d672218a1e15e65e0befe35
Trang <trangdaothu2005@gmail.com>
2026-03-14 13:52:42 +0700

    giao dien lich

---
+++ lib/giao_dien/screen_lich.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
+import 'package:btap_lon/giao_dien/screen_chi_tieu.dart';
+
+class ScreenLich extends StatefulWidget {
+  const ScreenLich({super.key});
+
+  @override
+  State<ScreenLich> createState() => _ScreenLichState();
+}
+
+class _ScreenLichState extends State<ScreenLich> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 98, 151, 194),
+        title: const Text('Sổ thu chi'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              //trang timf kiemse
+            },
+          ),
+          const Icon(Icons.more_vert),
+        ],
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2000, 1, 1),
+            lastDay: DateTime.utc(3000, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            calendarStyle: const CalendarStyle(
+              todayDecoration: BoxDecoration(
+                color: const Color.fromARGB(255, 98, 151, 194),
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: const Color.fromARGB(255, 98, 151, 194),
+                shape: BoxShape.circle,
+              ),
+            ),
+            headerStyle: const HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+            ),
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, date, events) {
+                return null;
+              },
+            ),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildSummaryItem('Tổng hiện có', '10.000.000', Colors.blue),
+                _buildSummaryItem('Tổng chi', '5.000.000', Colors.red),
+              ],
+            ),
+          ),
+          const Divider(),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildTransactionItem(
+                  'Ăn uống',
+                  'Phở bòo',
+                  '-35.000',
+                  Icons.restaurant,
+                  Colors.orange,
+                ),
+                _buildTransactionItem(
+                  'Xăng xe',
+                  'Đổ xăng',
+                  '-500.000',
+                  Icons.gas_meter,
+                  Colors.green,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        selectedItemColor: const Color.fromARGB(255, 98, 151, 194),
+        onTap: (index) {
+          if (index == 1) return;
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ScreenChiTieu()),
+            );
+          } else if (index == 2) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const ScreenBaoCao()),
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'Nhập vào'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Lịch',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart),
+            label: 'Báo cáo',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSummaryItem(String title, String amount, Color color) {
+    return Column(
+      children: [
+        Text(title, style: const TextStyle(color: Colors.grey)),
+        Text(
+          "$amount VND",
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTransactionItem(
+    String title,
+    String subtitle,
+    String amount,
+    IconData icon,
+    Color iconColor,
+  ) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: iconColor.withOpacity(0.1),
+        child: Icon(icon, color: iconColor),
+      ),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Text(
+        "$amount VND",
+        style: const TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:btap_lon/giao_dien/trang_dau.dart';
 import 'package:flutter/material.dart';
+import 'package:btap_lon/giao_dien/screen_chi_tieu.dart';
+import 'package:btap_lon/model/chi_tieu_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ChiTieuProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Quản lý chi tiêu',
+      theme: ThemeData(primarySwatch: Colors.amber, useMaterial3: true),
       home: TrangWidget(),
     );
   }
