d9690fd79020127d13e55d05e07b3afc22a07d0e
Thảo Vy <23010588@st.phenikaa-uni.edu.vn>
2026-03-14 11:15:47 +0700

    trang nhap vao

---
+++ 2025_2_LTTBDD_N02_Nhom_23-main/lib/model/chi_tieu.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChiTieu {
+  DateTime ngay;
+  String ghiChu;
+  double soTien;
+  String danhMuc;
+  IconData iconData;
+
+  ChiTieu({
+    required this.ngay,
+    required this.ghiChu,
+    required this.soTien,
+    required this.danhMuc,
+    required this.iconData,
+  });
+}
